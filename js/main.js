90144b651ff3c7cb510a8f5f81d57bd2132f1a30
Iamam305 <84980608+Iamam305@users.noreply.github.com>
2021-07-26 16:40:44 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+      let scroll = $(window).scrollTop();
+      if (scroll > 70) {
+        $(".nav-parent").addClass("nav-changeColor")
+      }
+      else {
+        $(".nav-parent").removeClass("nav-changeColor")
+      }
+    });
+  
+    $(".hamburger").click(function () {
+      $(".bar1").toggleClass("bar1-open")
+      $(".bar2").toggleClass("bar2-open")
+      $(".bar3").toggleClass("bar3-open")
+      $(".nav-menu").toggleClass("menu-open")
+  
+    })
+  
+    $(".nav-item").click(function () {
+  
+      $(".bar1").toggleClass("bar1-open")
+      $(".bar2").toggleClass("bar2-open")
+      $(".bar3").toggleClass("bar3-open")
+      $(".nav-menu").toggleClass("menu-open")
+    })
+  
+  
+  })
